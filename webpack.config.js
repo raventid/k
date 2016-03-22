25e9dfaf628454fbe50810b3e132e43c37c6eedb
raventid <juliankul@gmail.com>
2016-03-22 17:17:38 +0300

    settings up webpack in webpack.config, removes details from project.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const webpack = require('webpack')
 
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
@@ -19,7 +20,30 @@ const common = {
 
 //Default conf
 if(TARGET === 'start' || !TARGET){
-    module.exports = merge(common, {});
+    module.exports = merge(common, {
+        devServer: {
+            contentBase: PATHS.build,
+
+            //Enable history API fallback
+            // to let HTML5 History API based routing work.
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+
+            //Display only errors, don't show everything
+            stats: 'errors-only',
+
+            //Parse host and port from env so this is easy to customize
+            //For vagrant 
+            //host: process.ebv.HOST || '0.0.0.0';
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
 }
 
 if(TARGET === 'build'){
