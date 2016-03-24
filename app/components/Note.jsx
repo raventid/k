bd0d65344747bdcd01378083a86ab4c775512d45
raventid <juliankul@gmail.com>
2016-03-24 14:04:29 +0300

    brings fully working CRUD

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,61 @@
 import React from 'react';
 
-export default () => <div>Learn Webpack</div>;
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editing: false 
+    };
+  }
+  render() {
+    if(this.state.editing){
+      return this.renderEdit();
+    }
+
+    return this.renderNote();
+  }
+  renderEdit = () => {
+    return <input type="text"
+      ref={ (e) => e ? e.selectionStart = this.props.task.length : null }
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />;
+  };
+  renderNote = () => {
+    const onDelete = this.props.onDelete;
+
+    return (
+      <div onClick={this.edit}>
+        <span>{this.props.task}</span>
+        {onDelete ? this.renderDelete() : null}
+      </div>
+    );
+  };
+  edit = () => {
+    this.setState({
+      editing: true
+    });
+  };
+  checkEnter = (e) => {
+    if(e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  };
+  finishEdit = (e) => {
+    const value = e.target.value;
+
+    if(this.props.onEdit) {
+      this.props.onEdit(value);
+
+      /*exit edit mode*/
+      this.setState({
+        editing: false
+      });
+    }
+  };
+  renderDelete = () => {
+    return <button onClick={this.props.onDelete}>Delete</button>;
+  };
+}
